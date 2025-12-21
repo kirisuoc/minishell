050b6c19c596326bd09de1d396acfebfb7537342
kirisuoc <erikcousillas@icloud.com>
2025-12-21 01:39:28 +0100

    Now it works directly with commands without relative or absolute paths and show error messages for command not found and No directory found

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ecousill <ecousill@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 11:56:12 by ecousill          #+#    #+#             */
-/*   Updated: 2025/12/19 13:59:07 by ecousill         ###   ########.fr       */
+/*   Updated: 2025/12/21 01:37:57 by ecousill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,35 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <sys/wait.h>
+
+#define STDERR 2
+
+enum e_error_type {
+	ERR_CMD_NOT_FOUND,
+	ERR_NO_FILE,
+	ERR_SYSCALL
+};
+
+void	print_error(char *cmd, enum e_error_type type)
+{
+	if (type == ERR_CMD_NOT_FOUND) {
+		write(STDERR, "minishell: ", 11);
+		write(STDERR, cmd, ft_strlen(cmd));
+		write(STDERR, ": command not found\n", 20);
+	}
+	else if (type == ERR_NO_FILE) {
+		write(STDERR, "minishell: ", 11);
+		write(STDERR, cmd, ft_strlen(cmd));
+		write(STDERR, ": No such file or directory\n", 28);
+	}
+	else if (type == ERR_SYSCALL) {
+		// errno debe estar seteado por la syscall
+		write(STDERR, "minishell: ", 11);
+		write(STDERR, cmd, ft_strlen(cmd));
+		write(STDERR, ": ", 2);
+		perror(""); // perror añade la descripción del error
+	}
+}
 
 /*
 En mac:
@@ -46,15 +75,44 @@ char	**parse(char *line)
 
 	i = 0;
 	args = malloc(128 * sizeof(char *)); // espacio para 128 tokens
-	token = strtok(line, " \t\n");
+	token = strtok(line, " \t\n");										// Función no permitida, usar ft_split?
 	while (token != NULL)
 	{
 		args[i] = strdup(token); // duplicar string
 		i++;
-		token = strtok(NULL, " \t\n");
+		token = strtok(NULL, " \t\n");									// Función no permitida, usar ft_split?
 	}
 	args[i] = NULL; // execve necesita NULL al final
 	return args;
+}
+
+char *find_in_path(char *cmd, char **envp)
+{
+	char **paths;
+	char *full_path;
+	char *temp;
+
+	paths = ft_split(getenv("PATH"), ':');
+	for (int i = 0; paths[i]; i++){
+		temp = ft_strjoin(paths[i], "/");
+		full_path = ft_strjoin(temp, cmd);
+		free(temp);
+		if (access(full_path, X_OK) == 0)
+		{
+			for (int i = 0; paths[i]; i++){
+				free(paths[i]);
+			}
+			free(paths);
+			return (full_path);
+		}
+	}
+
+	for (int i = 0; paths[i]; i++){
+		free(paths[i]);
+	}
+	free(paths);
+
+	return (NULL);
 }
 
 void exec_command(char **args, char **envp)
@@ -72,18 +130,23 @@ void exec_command(char **args, char **envp)
 		{
 			if (execve(args[0], args, envp) == -1)
 			{
-				perror("execve");
+				print_error(args[0], ERR_NO_FILE);
+				//perror("execve");
 				exit(1);
 			}
 		}
-	/* 	else
+	 	else
 		{
-			if (execve(args[0], args, envp) == -1)
+			char *path = find_in_path(args[0], envp);
+			if (!path)
 			{
-				perror("execve");
-				exit(1);
+				print_error(args[0], ERR_CMD_NOT_FOUND);
+				//fprintf(stderr, "minishell: %s: command not found\n", args[0]);		// ???
+				exit(127);
 			}
-		} */
+			execve(path, args, envp);
+			free(path);
+		}
 	}
 	else
 	{
